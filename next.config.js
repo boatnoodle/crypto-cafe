787b3c32cc21b73c9de5f973ed697fae8cb3ea20
boatnoodle <nattasit31@gmail.com>
2021-12-16 06:42:40 +0700

    Initial project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require("next-compose-plugins");
+const withAntdLess = require("next-plugin-antd-less");
+const withTypescript = require("@zeit/next-typescript");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+
+const pluginAntdLess = withAntdLess({
+  lessVarsFilePath: "./assets/antd-custom.less",
+});
+
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  basePath: "",
+  webpack(config) {
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [[withTypescript], [withCSS], [withSass], [pluginAntdLess]],
+  nextConfig
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Button, ConfigProvider } from "antd";
+import { MoralisProvider } from "react-moralis";
+
+const APP_ID = process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isServerInfo = APP_ID && SERVER_URL ? true : false;
+
+  if (!isServerInfo)
+    throw new Error(
+      "Missing Moralis Application ID or Server URL. Make sure to set your .env file."
+    );
+
+  return (
+    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+      <ConfigProvider>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
